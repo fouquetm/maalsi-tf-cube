dccfee3de8356092f3a3144592e33936f710f0dd
Matthieu Fouquet <mfouquet@sqli.com>
2024-02-13 14:26:48 +0100

    add virtual network

--- main.tf
+++ main.tf
@@ -5,6 +5,20 @@ resource "azurerm_resource_group" "main" {
   tags = local.tags
 }
 
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-${var.project_name}"
+  address_space       = ["10.224.0.0/12"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = "sn-aks"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.224.0.0/16"]
+}
+
 resource "azurerm_kubernetes_cluster" "main" {
   name                = "aks-${var.project_name}"
   location            = azurerm_resource_group.main.location
@@ -20,6 +34,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     name       = "default"
     vm_size    = var.aks_nodes_vm_size
     enable_auto_scaling = true      # enable nodes auto-scaling
+    vnet_subnet_id = azurerm_subnet.aks.id  # attach to aks subnet. prevent from virtual network auto-creation
     node_count = 1
     min_count = 1
     max_count = 5
